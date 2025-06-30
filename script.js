1d1142f7ddfc5469cab40373730cc083f17ed65b
AlejandroParienteM <matt.shadows8603@gmail.com>
2025-06-30 00:30:02 -0400

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ class MatrixLove {
             "Drolicoco", "Mapirosa", "Amor", "Destino",
             "Radiante", "Amorcito", "Frutilla", "Reina",
             "Luz", "Encantadora", "Mapirosa", "Adorable",
-            , "Maravillosa", "Jhenisse", "Fascinante",
+            "Maravillosa", "Jhenisse", "Fascinante",
             "Cautivadora", "Inteligente", "Aliss", "Dulce",
             "Tierna", "Delicada", "Elegante", "Graciosa",
             "Alisson", "Preciosa", "Única", "Especial",
